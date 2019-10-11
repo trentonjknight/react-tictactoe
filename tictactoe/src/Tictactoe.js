613f0ded63a4e869d8cce04570c7a3c187224102
Trenton Knight <tknightj1995@yahoo.com>
2019-10-11 21:00:59 +0000

    setup

---
+++ tictactoe/src/Tictactoe.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="mainbox">
+        <div>
+            <div id="one" className="box1"></div>
+            <div id="two" className="box1"></div>
+            <div id="three" className="box1"></div>
+        </div>
+        <div>
+            <div id="four" className="box2"></div>
+            <div id="five" className="box2"></div>
+            <div id="six" className="box2"></div>
+        </div>
+        <div>
+            <div id="seven" className="box3"></div>
+            <div id="eight" className="box3"></div>
+            <div id="nine" className="box3"></div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
